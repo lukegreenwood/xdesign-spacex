d6bbdf0a56ea340430508014550522aa4b62daa5
Luke Greenwood <greenwood@bryter.io>
2022-07-20 17:42:24 +0100

    fix: add function for years

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import cx from "classnames";
 import { useLaunchContext } from "../../contexts/LaunchContext";
 
+const getDedupedYears = (items) => {
+  const allYears = items.map((launch) => {
+    const date = new Date(launch.launch_date_utc);
+    return date.getFullYear();
+  });
+  return Array.from(new Set(allYears));
+};
+
 export const Select = ({ label, classes, error, allowDisabledState }) => {
   const { setFilter, filter, items } = useLaunchContext();
   const selectClasses = cx(classes, {
     disabled: allowDisabledState ? error : "",
   });
 
-  const allYears = items.map((launch) => {
-    const date = new Date(launch.launch_date_utc);
-    return date.getFullYear();
-  });
-  const launchYears = Array.from(new Set(allYears));
+  const launchYears = getDedupedYears(items);
 
   return (
     <select
