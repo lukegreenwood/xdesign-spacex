96d670d324fd894bff931f03a0847afec0ef3b86
Luke Greenwood <greenwood@bryter.io>
2022-07-20 18:45:39 +0100

    style: add launchitem styles

--- src/components/LaunchItem/LaunchItem.jsx
+++ src/components/LaunchItem/LaunchItem.jsx
@@ -10,12 +10,12 @@ export const LaunchItem = ({ item, index }) => {
   } = item;
 
   return (
-    <li key={index}>
-      <div>
-        <span>{`#${flight_number}`}</span>
-        <span>{`${mission_name}`}</span>
+    <li key={index} className="launch-item">
+      <div className="launch-item__info">
+        <span className="launch-item__number">{`#${flight_number}`}</span>
+        <span className="launch-item__name">{`${mission_name}`}</span>
       </div>
-      <div>
+      <div className="launch-item__meta">
         <span>{moment(launch_date_utc).format("Do MMM YYYY")}</span>
         {rocket_name}
       </div>
