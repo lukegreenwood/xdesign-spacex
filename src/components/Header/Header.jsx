da20a71506b879f15cc540c83a027c5736a98963
Luke Greenwood <greenwood@bryter.io>
2022-07-20 18:35:52 +0100

    feat: add in onclick for refresh

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import { Button } from "../../components/Button";
 import { useLaunchContext } from "../../contexts/LaunchContext/LaunchContext";
 
 export const Header = () => {
-  const { filter } = useLaunchContext();
+  const { filter, listLaunches } = useLaunchContext();
   return (
     <header className="app__header">
       <div className="app__logo">
@@ -18,7 +18,7 @@ export const Header = () => {
       </div>
       <Button
         filter={filter}
-        //missing onClick
+        onClick={listLaunches}
         classes="button button--reload"
         label={LABEL.RELOAD}
         allowDisabledState={false}
