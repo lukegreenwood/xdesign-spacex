dbeef0a8644fcf176ed0bebe8e32d41ec7bc111a
Luke Greenwood <greenwood@bryter.io>
2022-07-20 17:34:36 +0100

    feat: get deduped list of years

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -3,10 +3,17 @@ import cx from "classnames";
 import { useLaunchContext } from "../../contexts/LaunchContext";
 
 export const Select = ({ label, classes, error, allowDisabledState }) => {
-  const { setFilter, filter } = useLaunchContext();
+  const { setFilter, filter, items } = useLaunchContext();
   const selectClasses = cx(classes, {
     disabled: allowDisabledState ? error : "",
   });
+
+  const allYears = items.map((launch) => {
+    const date = new Date(launch.launch_date_utc);
+    return date.getFullYear();
+  });
+  const launchYears = Array.from(new Set(allYears));
+
   return (
     <select
       name={label}
@@ -17,6 +24,11 @@ export const Select = ({ label, classes, error, allowDisabledState }) => {
       value={filter}
     >
       <option value="">{label}</option>
+      {launchYears.map((year) => (
+        <option key={year} value={year}>
+          {year}
+        </option>
+      ))}
     </select>
   );
 };
